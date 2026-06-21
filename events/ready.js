6d672d25cb9bc54391e3728a2584d0c25bdc625b
Luen404 <dongdongdong0801@gmail.com>
2026-06-22 00:32:43 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,40 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const loadCommands = require('../Handler/commandHandler');
-
-module.exports = {
-    name: 'clientReady',
-    once: true,
-    async execute(client) {
-        console.log(`로그인 => ${client.user.tag}`);
-
-        const dataPath = path.join(process.cwd(), 'data');
-
-        if (!fs.existsSync(dataPath)) {
-            fs.mkdirSync(dataPath, { recursive: true });
-            console.log('data 폴더 생성');
-        }
-
-        const files = [
-            'Inventory.json',
-            'Percent.json',
-            'PointConfig.json',
-            'users.json'
-        ];
-
-        for (const file of files) {
-            const filePath = path.join(dataPath, file);
-
-            if (!fs.existsSync(filePath)) {
-                fs.writeFileSync(
-                    filePath,
-                    JSON.stringify({}, null, 4),
-                    'utf8'
-                );
-                console.log(`${file} 생성`);
-            }
-        }
-
-        await loadCommands(client);
-    },
+const defaultData = {
+    'Inventory.json': {},
+    'Percent.json': {},
+    'PointConfig.json': {},
+    'users.json': {},
+    'shop.json' : {items:[]}
 };
+
+for (const [file, data] of Object.entries(defaultData)) {
+    const filePath = path.join(dataDir, file);
+
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(
+            filePath,
+            JSON.stringify(data, null, 4),
+            'utf8'
+        );
+    }
+}
