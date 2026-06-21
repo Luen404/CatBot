6673293762b6061c48f220557adc266bc401f86c
Luen404 <dongdongdong0801@gmail.com>
2026-06-22 00:36:07 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} 로그인 완료`);
+
+    const dataDir = path.join(__dirname, 'data');
+
+    // data 폴더 생성
+    if (!fs.existsSync(dataDir)) {
+        fs.mkdirSync(dataDir);
+        console.log('data 폴더 생성');
+    }
 const defaultData = {
     'Inventory.json': {},
     'Percent.json': {},
