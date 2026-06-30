c659fdf461cdcb55dc5715d5c1a90acaf1dc3134
Luen404 <dongdongdong0801@gmail.com>
2026-06-30 11:06:45 +0900

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,22 +1,17 @@
+const InteractionHandler = require("../blackjack/InteractionHandler");
+
 module.exports = {
-    name: 'interactionCreate',
-    once: false,
+    name: "interactionCreate",
     async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
 
-        const command = interaction.client.commands.get(interaction.commandName);
-        if (!command) return;
-
-        try {
-            await command.execute(interaction);
-        } catch(error) {
-            console.error(error)
-            const replyOptions = { content: '명령어 실행중 오류발생', ephmeral: true};
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followup(replyOptions);
-            }   else {
-                await interaction.reply(replyOptions);
-            }
+        if (interaction.isButton()) {
+            return InteractionHandler.handleButton(interaction);
         }
-    },
+
+        if (interaction.isChatInputCommand()) {
+            const cmd = interaction.client.commands.get(interaction.commandName);
+            if (!cmd) return;
+            return cmd.execute(interaction);
+        }
+    }
 };
