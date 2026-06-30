d36916c11ef82850b9b913a9f0ad1d71329b50b4
Luen404 <dongdongdong0801@gmail.com>
2026-06-30 09:25:55 +0900

    Create blackjack.js

---
+++ commands/blackjack.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("discord.js");
+const InteractionHandler = require("../../blackjack/InteractionHandler");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("블랙잭")
+        .setDescription("블랙잭 게임 패널 생성"),
+
+    async execute(interaction) {
+        return InteractionHandler.createPanel(interaction);
+    }
+};
