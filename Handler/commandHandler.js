b59599b83a22cbe8489e6716521cda77ca344983
Luen404 <dongdongdong0801@gmail.com>
2026-06-30 16:08:27 +0900

    Update commandHandler.js

--- Handler/commandHandler.js
+++ Handler/commandHandler.js
@@ -19,17 +19,21 @@ module.exports = async (client) => {
             console.log(`${filePath}에 data || execute 미포함`)
         }
     }
-    const rest = new REST({ version: '10'}).setToken(process.env.DSC_T);
-    try {
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {body : []},
-        )
-        console.log('슬래시 커맨드 등록중');
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {body:commandsData},
-        )
-    } catch (error) {
-        console.log('슬래시 커맨드 등록중 오류발생', error);
-    }
+    const rest = new REST({ version: '10' }).setToken(process.env.DSC_T);
+
+try {
+    console.log('슬래시 커맨드 등록중');
+
+    await rest.put(
+        Routes.applicationGuildCommands(
+            process.env.CLIENT_ID,
+            process.env.GUILD_ID
+        ),
+        { body: commandsData }
+    );
+
     console.log('커맨드 등록 완료');
+} catch (error) {
+    console.log('슬래시 커맨드 등록중 오류발생', error);
+}
 }
