aee02223790a0f3c616bdf3fd913cade2c7946d3
Luen404 <dongdongdong0801@gmail.com>
2026-06-30 09:08:12 +0900

    Update dealer.js

--- blackJack/dealer.js
+++ blackJack/dealer.js
@@ -1,76 +1,24 @@
-
 const Player = require("./Player");
 
 class Dealer extends Player {
     constructor() {
-        super(
-            {
-                id: "dealer",
-                username: "Dealer"
-            },
-            0
-        );
-
-        this.hideFirstCard = true;
+        super("dealer", "Dealer", 0);
     }
 
     shouldHit() {
-        return this.getScore() < 17;
+        return this.total < 17;
     }
 
-    reveal() {
-        this.hideFirstCard = false;
-    }
-
-    hide() {
-        this.hideFirstCard = true;
-    }
-
-    getVisibleCardsString() {
-        return this.cards
-            .map((card, index) => {
-                if (index === 0 && this.hideFirstCard) {
-                    return "`🂠`";
-                }
-
-                return card.display();
-            })
-            .join(" ");
-    }
-
-    getVisibleScore() {
-        if (!this.hideFirstCard) {
-            return this.getScore();
+    play(deck) {
+        while (this.shouldHit()) {
+            this.addCard(deck.draw());
         }
-
-        if (this.cards.length <= 1) {
-            return "?";
-        }
-
-        const visibleCards = this.cards.slice(1);
-
-        let total = 0;
-        let ace = 0;
-
-        for (const card of visibleCards) {
-            total += card.getValue();
-
-            if (card.rank === "A") {
-                ace++;
-            }
-        }
-
-        while (total > 21 && ace > 0) {
-            total -= 10;
-            ace--;
-        }
-
-        return total;
+        this.setStand();
     }
 
-    reset() {
-        super.reset();
-        this.hideFirstCard = true;
+    getVisibleHandString() {
+        if (this.hand.length === 0) return "";
+        return `${this.hand[0].toString()} ??`;
     }
 }
 
